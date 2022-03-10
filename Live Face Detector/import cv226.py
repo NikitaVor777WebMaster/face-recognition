d48eaa15690c8b2f10de27bfb5c289b82e8f4dd7
NikitaVor777WebMaster <101321170+NikitaVor777WebMaster@users.noreply.github.com>
2022-03-10 11:35:10 +0300

    Update import cv226.py

--- Live Face Detector/import cv226.py
+++ Live Face Detector/import cv226.py
@@ -2,7 +2,6 @@ import cv2 as cv
 
 capture = cv.VideoCapture(0) #to open Camera
 
-#accessing pretrained model
 pretrained_model = cv.CascadeClassifier("face_detector.xml") 
 
 while True:
@@ -11,14 +10,11 @@ while True:
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         coordinate_list = pretrained_model.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3) 
         
-        # drawing rectangle in frame
         for (x,y,w,h) in coordinate_list:
             cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
             
-        # Display detected face
         cv.imshow("Live Face Detection", frame)
         
-        # condition to break out of while loop
         if cv.waitKey(20) == ord('x'):
             break
         
